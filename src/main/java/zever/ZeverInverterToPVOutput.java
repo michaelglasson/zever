5d9dc4a35e40b5026ed6f48c381cd6a1100d702a
michaelglasson <mg@netspeed.com.au>
2020-01-13 07:37:18 +1100

    Separating into read inverter and post to PVOutput

--- src/main/java/zever/ZeverInverterToPVOutput.java
+++ src/main/java/zever/ZeverInverterToPVOutput.java
@@ -38,32 +38,5 @@ public class ZeverInverterToPVOutput {
 		Outputs p = new Outputs();
 		p.load();
 		p.updateOutputsWithInverterResponse(ZeverString);
-		
-		for (String s : p.outputs.keySet()) {
-			if (postToPVoutput(p.outputs.get(s).toCSV())) {
-				p.outputs.remove(s);
-			}
-		}
-		p.save();
-	}
-
-	public static boolean postToPVoutput(String inverterData) {
-		try {
-			Request req = Request.Post(props.getProperty("PVOutput.URL"));
-			req.version(HttpVersion.HTTP_1_1)
-	        .connectTimeout(1000)
-	        .socketTimeout(1000)
-			.addHeader("X-Pvoutput-Apikey", props.getProperty("PVOutput.APIKey"))
-			.addHeader("X-Pvoutput-SystemId", props.getProperty("PVOutput.SystemId"))
-			.bodyForm(Form.form().add("data", inverterData).build());
-			HttpResponse response = req.execute().returnResponse();
-			logger.info(response.getStatusLine().toString());
-			if (response.getStatusLine().getStatusCode() == 200) return true;
-		} catch (ClientProtocolException e) {
-			logger.error(e.getStackTrace());
-		} catch (IOException e) {
-			logger.error(e.getStackTrace());
-		}
-		return false;
 	}
 }
