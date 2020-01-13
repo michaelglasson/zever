38743f8ad408a11e16e7fd2933422dc6cee36840
michaelglasson <mg@netspeed.com.au>
2020-01-13 11:36:33 +1100

    Completed refactor

--- src/main/java/zever/Output.java
+++ src/main/java/zever/Output.java
@@ -15,8 +15,15 @@ public class Output {
 	
 	private static final Logger logger = LogManager.getLogger(Output.class);
 	
-	public Output() {}
-	
+	public Output() {} // Default constructor to allow creation of blank Output.
+
+	/*
+	 * This constructor takes an inverter response from the ZeverSolar inverter's home.cgi page and
+	 * fills the fields of this instance. It needs to adjust for the Zever error in the 
+	 * total generation for the day. "2.2" becomes "2.02", while "2.20" stays the same.
+	 * See smeyn and other discussion on this. We also need to convert it from KWH to WH.
+	 * The date time field in the Zever response is like "09:30 15/08/1957".
+	 */
 	public Output(String inverterResponse) {
 		String[] parts = inverterResponse.split("\n");
 		if (parts.length != 14) logger.error("Inverter response did not have 14 lines. Actually had: " + parts.length);
@@ -33,6 +40,10 @@ public class Output {
 		peak = parts[10];
 	}
 
+	/*
+	 * Note the empty field to align with the PVOutput data format. See the PVOutput API
+	 * documentation for details.
+	 */
 	public String toCSV() {
 		return String.format("%s,%s,,%s,%s", date, generated, peak, peakTime);
 	}
--- src/main/java/zever/Outputs.java
+++ src/main/java/zever/Outputs.java
@@ -1,32 +1,70 @@
 package zever;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.Properties;
 import java.util.TreeMap;
 import java.util.stream.Stream;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.fluent.Form;
+import org.apache.http.client.fluent.Request;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Outputs {
-	public Map<String, Output> outputs = new TreeMap<>();
+	public static Map<String, Output> outputs = new TreeMap<>();
 	private static final Logger logger = LogManager.getLogger(Outputs.class);
+	private static final Properties props = new Properties();
+	private static final String inverterResponse = "1\n" + "1\n" + "EAB96173XXXXXXX\n" + "NXVWWXSRRT7XXXXXXX\n"
+			+ "M11\n" + "16B21-663R+16B21-XXXXXX\n" + "11:00 31/08/2018\n" + // will ignore this and just use current
+																				// time
+			"OK\n" + "1\n" + "SX000660117XXXXXXXXX\n" + "2300\n" + // Assume this is watt hours)
+			"2.9\n" + // This needs to become 2.02 apparently
+			"OK\n" + "Error\n";
 
-	public void load() {
-		if (!Files.exists(Paths.get("output"))) return;
-		try (Stream<String> s = Files.lines(Paths.get("output"))) {
+	public Outputs() {
+		try (InputStream input = new FileInputStream("properties.txt")) {
+			props.load(input);
+		} catch (IOException e) {
+			logger.fatal(e.getMessage());
+			System.exit(1);
+		}
+	}
+
+	/*
+	 * This method reads power generation information from a ZeverSolar inverter and
+	 * puts it into an output file. Each time it is run, it checks the peak output
+	 * figure in the file against the output for the run and updates the peak output
+	 * and peak time if necessary. The output file contains any outputs that have
+	 * not yet been uploaded to PVOutput. In other words, the output file will have
+	 * today's output and any previous days' outputs that have not yet been
+	 * uploaded.
+	 */
+	public void collect() {
+		try (Stream<String> s = Files.lines(Paths.get(props.getProperty("LocalCacheFileName")))) {
 			s.forEach(l -> mapLine(l));
 		} catch (IOException e) {
 			logger.error("Cannot read the output file. " + e.getMessage());
 		}
+		logger.info("Inverter IP: " + props.getProperty("InverterURL"));
+		updateOutputsWithInverterResponse(inverterResponse);
 	}
 
-	public void save() {
-		try (PrintWriter pw = new PrintWriter(Paths.get("output").toFile())) {
+	/*
+	 * This writes the whole map to the outputs file, replacing whatever was
+	 * previously in there.
+	 */
+	private void save() {
+		try (PrintWriter pw = new PrintWriter(Paths.get(props.getProperty("LocalCacheFileName")).toFile())) {
 			for (String s : outputs.keySet()) {
 				pw.println(outputs.get(s).toCSV());
 			}
@@ -35,20 +73,32 @@ public class Outputs {
 		}
 	}
 
-	public void mapLine(String lineToMap) {
+	/*
+	 * Convert a line from the outputs file into an Output object. This is used when
+	 * reading the output file into the outputs map. This must happen when the
+	 * outputs file is updated.
+	 */
+	private void mapLine(String lineToMap) {
 		String[] s = lineToMap.split(",");
 		if (s.length != 5)
-			logger.error("Stored output in wrong format. It should have 5 parts, but has: " + s.length +
-					"Content is: " + lineToMap);
+			logger.error("Stored output in wrong format. It should have 5 parts, but has: " + s.length + "Content is: "
+					+ lineToMap);
 		Output o = new Output();
 		o.date = s[0];
 		o.generated = s[1];
-		o.peak = s[2];
-		o.peakTime = s[3];
+		o.peak = s[3];
+		o.peakTime = s[4];
 		outputs.put(o.date, o);
 	}
 
-	public void updateOutputsWithInverterResponse(String inverterResponse) {
+	/*
+	 * This updates the output file, either adding a new line or updating the total
+	 * generation and (if required) peak output and output time. It only updates the
+	 * output for the current day. This prepares the output file for posting to
+	 * PVOutput. The whole outputs file is overwritten with the current data from
+	 * the outputs map when save() is executed.
+	 */
+	private void updateOutputsWithInverterResponse(String inverterResponse) {
 		Output o = new Output(inverterResponse);
 		if (outputs.containsKey(o.date)) {
 			if (Integer.parseInt(outputs.get(o.date).peak) < Integer.parseInt(o.peak)) {
@@ -60,5 +110,48 @@ public class Outputs {
 			outputs.put(o.date, o);
 		}
 		save();
+	}
+
+	/*
+	 * For each output in the collection, try to post it to PVOutput. Delete each
+	 * output when it is posted successsfully. This means that the output file
+	 * should only contain unposted data. Data should therefore only be written once
+	 * per day after total day's generation, peak output and peak output time are
+	 * known. If there is a failure, the output remains in the file and can be
+	 * posted in the next run.
+	 */
+	public void postToPVOutput() {
+		for (String s : outputs.keySet()) {
+			if (doPost(outputs.get(s).toCSV())) {
+				outputs.remove(s);
+			}
+		}
+		save();
+	}
+
+	/*
+	 * Post inverter output to PVOutput. This is called once for each output. The
+	 * fields provided are date, generated, peak and peak time. API key is in the
+	 * properties file. Inverter data is generated by the toCSV method of the Output
+	 * class. The PVOutput API is a little ambiguous in that it does not specify
+	 * that the data should be in a form.
+	 */
+	private boolean doPost(String inverterData) {
+		try {
+			Request req = Request.Post(props.getProperty("PVOutput.URL"));
+			req.version(HttpVersion.HTTP_1_1).connectTimeout(1000).socketTimeout(1000)
+					.addHeader("X-Pvoutput-Apikey", props.getProperty("PVOutput.APIKey"))
+					.addHeader("X-Pvoutput-SystemId", props.getProperty("PVOutput.SystemId"))
+					.bodyForm(Form.form().add("data", inverterData).build());
+			HttpResponse response = req.execute().returnResponse();
+			logger.info(response.getStatusLine().toString());
+			if (response.getStatusLine().getStatusCode() == 200)
+				return true;
+		} catch (ClientProtocolException e) {
+			logger.error(e.getStackTrace());
+		} catch (IOException e) {
+			logger.error(e.getStackTrace());
+		}
+		return false;
 	}
 }
